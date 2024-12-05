d02fcc31e19ba7898da11ac3025bf2d53a4841ef
arjun r <arjun.rdell@gmail.com>
2024-12-05 20:15:01 +0530

    added sign up functionality

--- App/Screens/Login.tsx
+++ App/Screens/Login.tsx
@@ -1,18 +1,28 @@
 import { View, TextInput, Image, Text, TouchableOpacity, StyleSheet } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Login() {
+    const[login, setLogin] = useState(true);
+
     return (
         <View style={style.container}>
-            <Image style={{ marginBottom: 10 }} source={require("../Assets/bloodReport.png")} />
-            <Text style={{ color: "white", fontSize: 40, fontWeight: "bold", marginBottom: 10 }}>Login</Text>
+            <Image style={{ marginBottom: 10,width:"60%", resizeMode:"contain" }} source={require("../Assets/bloodReport.png")} />
+            <Text style={{ color: "white", fontSize: 35, fontWeight: "bold", marginBottom: 10 }}>{login ? "Login":"Sign Up"}</Text>
             <View style={{ width: "100%", alignItems: "center" }}>
+                {login ? <></>:<TextInput style={style.inputForm} placeholder='Name' />}
+                {login ? <></>:<TextInput style={style.inputForm} placeholder='Gender' />}
                 <TextInput style={style.inputForm} placeholder='Email' />
                 <TextInput secureTextEntry style={style.inputForm} placeholder='Password' />
             </View>
             <TouchableOpacity>
-                <Text style={{ paddingHorizontal: 16, paddingVertical: 8, borderRadius: 10, backgroundColor: "#616161", color: "white", fontSize: 24 }}>Login</Text>
+                <Text style={{ paddingHorizontal: 16, paddingVertical: 8, borderRadius: 10, backgroundColor: "#616161", color: "white", fontSize: 24 }}>{login ? "Login":"Sign Up"}</Text>
             </TouchableOpacity>
+            <TouchableOpacity>
+                <Text onPress={()=> login ? setLogin(false):setLogin(true)} style={{color:"white", marginTop:10}}>{login ? "Don't have an Account":"Already have an Account"}</Text>
+            </TouchableOpacity>
+            {login ? <TouchableOpacity>
+                <Text style={{color:"white", marginTop:10}}>Forgot Password</Text>
+            </TouchableOpacity>:<></>}
         </View>
     )
 }
@@ -25,11 +35,11 @@ const style = StyleSheet.create({
         fontFamily:"Ubuntu"
     },
     inputForm: {
-        fontSize: 24,
-        height: 50,
-        color: "white",
+        fontSize: 18,
+        height: 40,
         width: "80%",
-        backgroundColor: "#616161",
+        color:"black",
+        backgroundColor: "white",
         borderRadius: 10,
         paddingHorizontal: 10,
         paddingVertical: 5,
